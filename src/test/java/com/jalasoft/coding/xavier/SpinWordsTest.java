bce3f3747840ca8a36fa77ae29192b3fc60889b8
XavierOropeza <xoropeza93@gmail.com>
2021-04-30 20:53:15 -0400

    adding spingwords by xavier

---
+++ src/test/java/com/jalasoft/coding/xavier/SpinWordsTest.java
@@ -0,0 +1,16 @@
+package com.jalasoft.coding.xavier;
+
+public class SpinWordsTest { public String spinWords(String sentence) {
+    StringBuilder sb = new StringBuilder();
+    String [] splitSentence = sentence.split(" ");
+
+    for (String word : splitSentence) {
+        if (word.length() > 5) {
+            sb.append(new StringBuilder(word).reverse().toString() + " ");
+        } else {
+            sb.append(word + " ");
+        }
+    }
+    return sb.toString().trim();
+}
+}
