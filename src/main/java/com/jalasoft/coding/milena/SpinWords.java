5fb6905cecddd2f40d20f42046c6c988d7f95b40
Milena Vallejos <Milena.Vallejos@jalasoft.com>
2021-05-03 20:01:09 -0400

    Fixed codding style errors

--- src/main/java/com/jalasoft/coding/milena/SpinWords.java
+++ src/main/java/com/jalasoft/coding/milena/SpinWords.java
@@ -1,6 +1,29 @@
 package com.jalasoft.coding.milena;
-public final class SpinWords {
-    public static String spinWords( String phrase) {
+
+/**
+ * This class permits to spin the word that have more than 5 characters.
+ */
+public class SpinWords {
+    /**
+     * This is the limit of words that should be spin.
+     */
+    private static final int SPIN_WORD_LIMIT = 5;
+
+    /**
+     * This is the default constructor.
+     * @param copyPhrase this is the phrase to be Spin.
+     */
+    SpinWords(final String copyPhrase) {
+
+
+    }
+
+    /**
+     * Spin the characters for a phrase.
+     * @param phrase this is the phrase to be Spin
+     * @return the spin phrase
+     */
+    public String spinWords(final String phrase) {
 
         String words = "";
         /* Here  splits a string in several strings based on the
@@ -13,10 +36,11 @@ public final class SpinWords {
             return words;
         }
         // Here we spin the word only if the characters are more than 5
-        for (int i = 0; i < phaseSplited.length; i++)
-            if (!(phaseSplited[i].length() <= 5)) {
+        for (int i = 0; i < phaseSplited.length; i++) {
+            if (!(phaseSplited[i].length() <= SPIN_WORD_LIMIT)) {
                 phaseSplited[i] = new StringBuilder(phaseSplited[i]).reverse().toString();
             }
+        }
         words = String.join(" ", phaseSplited);
         return words;
     }
--- src/test/java/com/jalasoft/coding/milena/SpinWordsTests.java
+++ src/test/java/com/jalasoft/coding/milena/SpinWordsTests.java
@@ -13,8 +13,9 @@ public class SpinWordsTests {
      */
     @Test
     public void spinWordsEmptyStringTest() {
+        var spinWords = new SpinWords("");
         String input = "";
-        String actual = SpinWords.spinWords(input);
+        String actual = spinWords.spinWords(input);
         String expected = "";
         Assert.assertEquals(actual, expected);
     }
@@ -25,41 +26,45 @@ public class SpinWordsTests {
 
     @Test
     public void spinWordSingleLetterTest() {
+        var spinWords = new SpinWords("");
         String input = "A";
-        String actual = SpinWords.spinWords(input);
+        String actual = spinWords.spinWords(input);
         String expected = "A";
         Assert.assertEquals(actual, expected);
     }
 
     /**
-     * Test to spin is not executed with a word with two Characters
+     * Test to spin is not executed with a word with two Characters.
      */
     @Test
     public void spinWordOnlyTwoLetterTest() {
+        var spinWords = new SpinWords("");
         String input = "Se";
-        String actual = SpinWords.spinWords(input);
+        String actual = spinWords.spinWords(input);
         String expected = "Se";
         Assert.assertEquals(actual, expected);
     }
 
     /**
-     * Test to spin is not executed for a word less than 5 characters
+     * Test to spin is not executed for a word less than 5 characters.
      */
     @Test
     public void spinWordsSingleWordLessThan5Test() {
+        var spinWords = new SpinWords("");
         String input = "Test";
-        String actual = SpinWords.spinWords(input);
+        String actual = spinWords.spinWords(input);
         String expected = "Test";
         Assert.assertEquals(actual, expected);
     }
 
     /**
-     * Test to spin string with a word with more than 5 characters
+     * Test to spin string with a word with more than 5 characters.
      */
     @Test
     public void spinWordsSingleWordMoreThan5Test() {
+        var spinWords = new SpinWords("");
         String input = "Follow";
-        String actual = SpinWords.spinWords(input);
+        String actual = spinWords.spinWords(input);
         String expected = "wolloF";
         Assert.assertEquals(actual, expected);
     }
@@ -69,8 +74,9 @@ public class SpinWordsTests {
      */
     @Test
     public void spinWordsPhraseTest() {
+        var spinWords = new SpinWords("");
         String input = "Hey fellow warriors";
-        String actual = SpinWords.spinWords(input);
+        String actual = spinWords.spinWords(input);
         String expected = "Hey wollef sroirraw";
         Assert.assertEquals(actual, expected);
     }
