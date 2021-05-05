b11575f6103979a3089e64de383284cf1c19a64b
XavierOropeza <xavier.oropeza@jalasoft.com>
2021-05-04 20:12:50 -0400

    cambio 2

--- src/test/java/com/jalasoft/coding/xavier/SpinWordsTest.java
+++ src/test/java/com/jalasoft/coding/xavier/SpinWordsTest.java
@@ -1,16 +1,50 @@
 package com.jalasoft.coding.xavier;
 
-public class SpinWordsTest { public String spinWords(String sentence) {
-    StringBuilder sb = new StringBuilder();
-    String [] splitSentence = sentence.split(" ");
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-    for (String word : splitSentence) {
-        if (word.length() > 5) {
-            sb.append(new StringBuilder(word).reverse().toString() + " ");
-        } else {
-            sb.append(word + " ");
-        }
+/**
+ *
+ */
+public class SpinWordsTest {
+    /**
+     * Test to spin empty string.
+     */
+    @Test
+    public void spinWordsEmptyStringTest() {
+        String input = "";
+        String actual = SpinWords.spinWords(input);
+        String expected = "";
+        Assert.assertEquals(actual, expected);
     }
-    return sb.toString().trim();
-}
+    /**
+     * Test to spin single word string with less than 5 of length.
+     */
+    @Test
+    public void spinWordsSingleWordLessThan5Test() {
+        String input = "Word";
+        String actual = SpinWords.spinWords(input);
+        String expected = "Word";
+        Assert.assertEquals(actual, expected);
+    }
+    /**
+     * Test to spin single word string with more than 5 of length.
+     */
+    @Test
+    public void spinWordsSingleWordMoreThan5Test() {
+        String input = "Spinword";
+        String actual = SpinWords.spinWords(input);
+        String expected = "drownipS";
+        Assert.assertEquals(actual, expected);
+    }
+    /**
+     * Test to spin multiple words string.
+     */
+    @Test
+    public void spinWordsMultipleWordsTest() {
+        String input = "Hello this is Shayra's tests";
+        String actual = SpinWords.spinWords(input);
+        String expected = "olleH this is s'aryahS stset";
+        Assert.assertEquals(actual, expected);
+    }
 }
